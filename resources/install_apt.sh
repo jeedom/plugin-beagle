0ec565e4bd8e31b43ec1836a9dee52ed3bbdf067
sarakha63 <sarakha_ludovic@yahoo.fr>
2019-07-09 14:31:29 +0200

    added wheel

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -8,18 +8,22 @@ echo "********************************************************"
 echo "*             Installation des dépendances             *"
 echo "********************************************************"
 sudo apt-get update
-echo 50 > ${PROGRESS_FILE}
+echo 10 > ${PROGRESS_FILE}
 sudo apt-get -y install python3-pip python3-setuptools
-echo 60 > ${PROGRESS_FILE}
+echo 20 > ${PROGRESS_FILE}
 sudo pip3 install requests
-echo 75 > ${PROGRESS_FILE}
+echo 35 > ${PROGRESS_FILE}
 sudo pip3 install pyserial
-echo 85 > ${PROGRESS_FILE}
+echo 45 > ${PROGRESS_FILE}
 sudo pip3 install pyudev
+echo 75 > ${PROGRESS_FILE}
+sudo pip3 install wheel
+echo 80 > ${PROGRESS_FILE}
+sudo pip3 install cryptography
+echo 99 > ${PROGRESS_FILE}
 sudo hciconfig hci0 up >/dev/null 2>&1
 sudo hciconfig hci1 up >/dev/null 2>&1
 sudo hciconfig hci2 up >/dev/null 2>&1
-sudo pip3 install cryptography
 echo 100 > ${PROGRESS_FILE}
 echo "********************************************************"
 echo "*             Installation terminée                    *"
