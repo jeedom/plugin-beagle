0091d8467879e2e2708ce97ef759d97d86e5ed83
Mips2648 <mips2648@gmail.com>
2025-01-14 10:41:01 +0100

    patch pybluez for deb12

--- resources/post_install.sh
+++ resources/post_install.sh
@@ -1,4 +1,13 @@
-sudo pip3 install git+https://github.com/pybluez/pybluez.git#egg=pybluez
+cd "$(dirname "$0")"
+SCRIPT_DIR="$(pwd)"
+
+if [ -d "$SCRIPT_DIR/python_venv/bin" ]; then
+  echo "patch pybluez on debian >= 12"
+  sudo $SCRIPT_DIR/python_venv/bin/python3 -m pip install --force-reinstall --upgrade git+https://github.com/pybluez/pybluez.git#egg=pybluez
+else
+  echo "patch pybluez on debian 11"
+  sudo pip3 install git+https://github.com/pybluez/pybluez.git#egg=pybluez
+fi
 
 sudo rfkill unblock all >/dev/null 2>&1
 sudo hciconfig hci0 up >/dev/null 2>&1
