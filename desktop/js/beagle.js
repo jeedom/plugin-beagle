7ed6dc3a7a9135957bcdedbc0c5996719bd89a87
Mips2648 <mips2648@gmail.com>
2025-01-15 09:27:25 +0100

    adapt cmd list for v4

--- desktop/js/beagle.js
+++ desktop/js/beagle.js
@@ -68,8 +68,16 @@ function addCmdToTable(_cmd) {
   var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
   tr += '<td>';
   tr += '<span class="cmdAttr" data-l1key="id" style="display:none;"></span>';
-  tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" style="width : 140px;" placeholder="{{Nom}}">';
+  tr += '<div class="input-group">';
+  tr += '<input class="cmdAttr form-control input-sm roundedLeft" data-l1key="name" placeholder="{{Nom de la commande}}">';
+  tr += '<span class="input-group-btn"><a class="cmdAction btn btn-sm btn-default" data-l1key="chooseIcon" title="{{Choisir une icône}}"><i class="fas fa-icons"></i></a></span>';
+  tr += '<span class="cmdAttr input-group-addon roundedRight" data-l1key="display" data-l2key="icon" style="font-size:19px;padding:0 5px 0 0!important;"></span>';
+  tr += '</div>';
+  tr += '<select class="cmdAttr form-control input-sm" data-l1key="value" style="display:none;margin-top:5px;" title="{{Commande info liée}}">';
+  tr += '<option value="">{{Aucune}}</option>';
+  tr += '</select>';
   tr += '</td>';
+
   tr += '<td>';
   tr += '<span class="type" type="' + init(_cmd.type) + '">' + jeedom.cmd.availableType() + '</span>';
   tr += '<span class="subType" subType="' + init(_cmd.subType) + '"></span>';
@@ -84,20 +92,34 @@ function addCmdToTable(_cmd) {
   tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" checked/>{{Historiser}}</label></span> ';
   tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr" data-l1key="display" data-l2key="invertBinary"/>{{Inverser}}</label></span> ';
   tr += '</td>';
+
+  tr += '<td>';
+  tr += '<span class="cmdAttr" data-l1key="htmlstate"></span>';
+  tr += '</td>';
+
   tr += '<td>';
   if (is_numeric(_cmd.id)) {
     tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fa fa-cogs"></i></a> ';
     tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fa fa-rss"></i> {{Tester}}</a>';
   }
-  tr += '<i class="fa fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i>';
+  tr += '<i class="fas fa-minus-circle pull-right cmdAction cursor" data-action="remove" title="{{Supprimer la commande}}"></i></td>'
   tr += '</td>';
   tr += '</tr>';
+
   $('#table_cmd tbody').append(tr);
-  $('#table_cmd tbody tr:last').setValues(_cmd, '.cmdAttr');
-  if (isset(_cmd.type)) {
-    $('#table_cmd tbody tr:last .cmdAttr[data-l1key=type]').value(init(_cmd.type));
-  }
-  jeedom.cmd.changeType($('#table_cmd tbody tr:last'), init(_cmd.subType));
+  var tr = $('#table_cmd tbody tr').last();
+  jeedom.eqLogic.buildSelectCmd({
+    id: $('.eqLogicAttr[data-l1key=id]').value(),
+    filter: { type: 'info' },
+    error: function (error) {
+      $('#div_alert').showAlert({ message: error.message, level: 'danger' })
+    },
+    success: function (result) {
+      tr.find('.cmdAttr[data-l1key=value]').append(result);
+      tr.setValues(_cmd, '.cmdAttr');
+      jeedom.cmd.changeType(tr, init(_cmd.subType));
+    }
+  })
 }
 
 $('body').on('beagle::includeState', function (_event, _options) {
--- desktop/php/beagle.php
+++ desktop/php/beagle.php
@@ -5,9 +5,7 @@ if (!isConnect('admin')) {
 $plugin = plugin::byId('beagle');
 sendVarToJS('eqType', $plugin->getId());
 $eqLogics = eqLogic::byType($plugin->getId());
-function sortByOption($a, $b) {
-  return strcmp($a['name'], $b['name']);
-}
+
 if (config::byKey('include_mode', 'beagle', 0) == 1) {
   echo '<div class="alert jqAlert alert-warning" id="div_inclusionAlert" style="margin : 0px 5px 15px 15px; padding : 7px 35px 7px 15px;">{{Vous êtes en mode scan. Recliquez sur le bouton scan pour sortir de ce mode (sinon le mode restera actif une minute)}}</div>';
 } else {
@@ -15,128 +13,142 @@ if (config::byKey('include_mode', 'beagle', 0) == 1) {
 }
 ?>
 
-<div class="col-lg-12 eqLogicThumbnailDisplay">
-  <legend><i class="fas fa-cog"></i> {{Gestion}}</legend>
-  <div class="eqLogicThumbnailContainer">
+<div class="row row-overflow">
+  <div class="col-xs-12 eqLogicThumbnailDisplay">
+    <legend><i class="fas fa-cog"></i> {{Gestion}}</legend>
+    <div class="eqLogicThumbnailContainer">
+      <?php
+      if (config::byKey('include_mode', 'beagle', 0) == 1) {
+        echo '<div class="cursor changeIncludeState include card logoPrimary" data-mode="1" data-state="0" >';
+        echo '<i class="fa fa-spinner fa-pulse"></i>';
+        echo '<br/>';
+        echo '<span>{{Arrêter scan}}</span>';
+        echo '</div>';
+      } else {
+        echo '<div class="cursor changeIncludeState include card logoPrimary " data-mode="1" data-state="1">';
+        echo '<i class="fa fa-bullseye"></i>';
+        echo '<br/>';
+        echo '<span>{{Lancer scan}}</span>';
+        echo '</div>';
+      }
+      ?>
+      <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
+        <i class="fas fa-wrench"></i>
+        <br />
+        <span>{{Configuration}}</span>
+      </div>
+      <div class="cursor logoSecondary" id="bt_healthbeagle">
+        <i class="fas fa-medkit"></i>
+        <br />
+        <span>{{Santé}}</span>
+      </div>
+      <div class="cursor logoSecondary" id="bt_askscenes">
+        <i class="fas fa-image"></i>
+        <br />
+        <span>{{Scènes}}</span>
+      </div>
+      <div class="cursor logoSecondary" id="bt_askgroups">
+        <i class="fas fa-list-alt"></i>
+        <br />
+        <span>{{Groupes}}</span>
+      </div>
+    </div>
     <?php
-    if (config::byKey('include_mode', 'beagle', 0) == 1) {
-      echo '<div class="cursor changeIncludeState include card logoPrimary" data-mode="1" data-state="0" >';
-      echo '<i class="fa fa-spinner fa-pulse"></i>';
-      echo '<br/>';
-      echo '<span>{{Arrêter Scan}}</span>';
-      echo '</div>';
+    if (count($eqLogics) == 0) {
+      echo '<br><div class="text-center" style="font-size:1.2em;font-weight:bold;">{{Aucun équipement trouvé, cliquer sur "Lancer le scan" pour commencer}}</div>';
     } else {
-      echo '<div class="cursor changeIncludeState include card logoPrimary " data-mode="1" data-state="1">';
-      echo '<i class="fa fa-bullseye"></i>';
-      echo '<br/>';
-      echo '<span>{{Lancer Scan}}</span>';
+      // Champ de recherche
+      echo '<div class="input-group" style="margin:5px;">';
+      echo '<input class="form-control roundedLeft" placeholder="{{Rechercher}}" id="in_searchEqlogic">';
+      echo '<div class="input-group-btn">';
+      echo '<a id="bt_resetSearch" class="btn" style="width:30px"><i class="fas fa-times"></i></a>';
+      echo '<a class="btn roundedRight hidden" id="bt_pluginDisplayAsTable" data-coreSupport="1" data-state="0"><i class="fas fa-grip-lines"></i></a>';
       echo '</div>';
-    }
+      echo '</div>';
     ?>
-    <div class="cursor eqLogicAction logoSecondary" data-action="gotoPluginConf">
-      <i class="fas fa-wrench"></i>
-      <br />
-      <span>{{Configuration}}</span>
-    </div>
-    <div class="cursor logoSecondary" id="bt_healthbeagle">
-      <i class="fas fa-medkit"></i>
-      <br />
-      <span>{{Santé}}</span>
-    </div>
-    <div class="cursor logoSecondary" id="bt_askscenes">
-      <i class="fas fa-image"></i>
-      <br />
-      <span>{{Scènes}}</span>
-    </div>
-    <div class="cursor logoSecondary" id="bt_askgroups">
-      <i class="fas fa-list-alt"></i>
-      <br />
-      <span>{{Groupes}}</span>
-    </div>
-  </div>
-  <div class="input-group" style="margin:5px;">
-    <input class="form-control" placeholder="{{Rechercher}}" id="in_searchEqlogic" />
-    <div class="input-group-btn">
-      <a id="bt_resetSearch" class="btn roundedRight" style="width:30px"><i class="fas fa-times"></i></a>
-    </div>
-  </div>
-  <legend><i class="fas fa-table"></i> {{Mes devices Odace SFSP}}</legend>
-  <div class="eqLogicThumbnailContainer">
-    <?php
-    foreach ($eqLogics as $eqLogic) {
-      if (!in_array(substr($eqLogic->getConfiguration('device', ''), 0, 5), array('scene', 'group'))) {
-        $opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
-        echo '<div class="eqLogicDisplayCard cursor ' . $opacity . '" data-eqLogic_id="' . $eqLogic->getId() . '">';
-        if (file_exists(__DIR__ . '/../../core/config/devices/' . $eqLogic->getConfiguration('device') . '/' . $eqLogic->getConfiguration('device') . '.png')) {
-          echo '<img class="lazy" src="plugins/beagle/core/config/devices/' . $eqLogic->getConfiguration('device') . '/' . $eqLogic->getConfiguration('device') . '.png"/>';
-        } else {
-          echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+      <legend><i class="fas fa-table"></i> {{Mes devices Odace SFSP}}</legend>
+      <div class="eqLogicThumbnailContainer">
+        <?php
+        foreach ($eqLogics as $eqLogic) {
+          if (!in_array(substr($eqLogic->getConfiguration('device', ''), 0, 5), array('scene', 'group'))) {
+            $opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+            echo '<div class="eqLogicDisplayCard cursor ' . $opacity . '" data-eqLogic_id="' . $eqLogic->getId() . '">';
+            if (file_exists(__DIR__ . '/../../core/config/devices/' . $eqLogic->getConfiguration('device') . '/' . $eqLogic->getConfiguration('device') . '.png')) {
+              echo '<img class="lazy" src="plugins/beagle/core/config/devices/' . $eqLogic->getConfiguration('device') . '/' . $eqLogic->getConfiguration('device') . '.png"/>';
+            } else {
+              echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+            }
+            echo '<br/>';
+            echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+            echo '</div>';
+          }
         }
-        echo '<br/>';
-        echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
-        echo '</div>';
-      }
-    }
-    ?>
-  </div>
-  <legend><i class="fas fa-image"></i> {{Mes scènes Odace SFSP}}</legend>
-  <div class="eqLogicThumbnailContainer">
-    <?php
-    foreach ($eqLogics as $eqLogic) {
-      if (in_array($eqLogic->getConfiguration('device', ''), array('scene'))) {
-        $opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
-        echo '<div class="eqLogicDisplayCard cursor ' . $opacity . '" data-eqLogic_id="' . $eqLogic->getId() . '">';
-        if (file_exists(__DIR__ . '/../../core/config/devices/' . $eqLogic->getConfiguration('device') . '/' . $eqLogic->getConfiguration('device') . '.png')) {
-          echo '<img class="lazy" src="plugins/beagle/core/config/devices/' . $eqLogic->getConfiguration('device') . '/' . $eqLogic->getConfiguration('device') . '.png"/>';
-        } else {
-          echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+        ?>
+      </div>
+      <legend><i class="fas fa-image"></i> {{Mes scènes Odace SFSP}}</legend>
+      <div class="eqLogicThumbnailContainer">
+        <?php
+        foreach ($eqLogics as $eqLogic) {
+          if (in_array($eqLogic->getConfiguration('device', ''), array('scene'))) {
+            $opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+            echo '<div class="eqLogicDisplayCard cursor ' . $opacity . '" data-eqLogic_id="' . $eqLogic->getId() . '">';
+            if (file_exists(__DIR__ . '/../../core/config/devices/' . $eqLogic->getConfiguration('device') . '/' . $eqLogic->getConfiguration('device') . '.png')) {
+              echo '<img class="lazy" src="plugins/beagle/core/config/devices/' . $eqLogic->getConfiguration('device') . '/' . $eqLogic->getConfiguration('device') . '.png"/>';
+            } else {
+              echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+            }
+            echo '<br/>';
+            echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+            echo '</div>';
+          }
         }
-        echo '<br/>';
-        echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
-        echo '</div>';
-      }
-    }
-    ?>
-  </div>
-  <legend><i class="fas fa-list-alt"></i> {{Mes groupes Odace SFSP}}</legend>
-  <div class="eqLogicThumbnailContainer">
-    <?php
-    foreach ($eqLogics as $eqLogic) {
-      if (in_array(substr($eqLogic->getConfiguration('device', ''), 0, 5), array('group'))) {
-        $opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
-        echo '<div class="eqLogicDisplayCard cursor ' . $opacity . '" data-eqLogic_id="' . $eqLogic->getId() . '">';
-        if (file_exists(__DIR__ . '/../../core/config/devices/' . $eqLogic->getConfiguration('device') . '/' . $eqLogic->getConfiguration('device') . '.png')) {
-          echo '<img class="lazy" src="plugins/beagle/core/config/devices/' . $eqLogic->getConfiguration('device') . '/' . $eqLogic->getConfiguration('device') . '.png"/>';
-        } else {
-          echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+        ?>
+      </div>
+      <legend><i class="fas fa-list-alt"></i> {{Mes groupes Odace SFSP}}</legend>
+      <div class="eqLogicThumbnailContainer">
+        <?php
+        foreach ($eqLogics as $eqLogic) {
+          if (in_array(substr($eqLogic->getConfiguration('device', ''), 0, 5), array('group'))) {
+            $opacity = ($eqLogic->getIsEnable()) ? '' : 'disableCard';
+            echo '<div class="eqLogicDisplayCard cursor ' . $opacity . '" data-eqLogic_id="' . $eqLogic->getId() . '">';
+            if (file_exists(__DIR__ . '/../../core/config/devices/' . $eqLogic->getConfiguration('device') . '/' . $eqLogic->getConfiguration('device') . '.png')) {
+              echo '<img class="lazy" src="plugins/beagle/core/config/devices/' . $eqLogic->getConfiguration('device') . '/' . $eqLogic->getConfiguration('device') . '.png"/>';
+            } else {
+              echo '<img src="' . $plugin->getPathImgIcon() . '"/>';
+            }
+            echo '<br/>';
+            echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
+            echo '</div>';
+          }
         }
-        echo '<br/>';
-        echo '<span class="name">' . $eqLogic->getHumanName(true, true) . '</span>';
-        echo '</div>';
-      }
+        ?>
+      </div>
+    <?php
     }
     ?>
   </div>
-</div>
 
-<div class="col-lg-12 eqLogic" style="display: none;">
-  <div class="input-group pull-right" style="display:inline-flex">
-    <span class="input-group-btn">
-      <a class="btn btn-danger btn-sm roundedLeft" id="bt_autoDetectModule"><i class="fas fa-search" title="{{Recréer les commandes}}"></i> {{Recréer les commandes}}</a><a class="btn btn-warning btn-sm haspairing" id="bt_pairing"><i class="fas fa-barcode" title="{{Trame pairing}}"></i> {{Trame pairing}}</a><a class="btn btn-default btn-sm eqLogicAction" data-action="configure"><i class="fas fa-cogs"></i> {{Configuration avancée}}</a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
-    </span>
-  </div>
-  <ul class="nav nav-tabs" role="tablist">
-    <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fas fa-arrow-circle-left"></i></a></li>
-    <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
-    <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a></li>
-  </ul>
-  <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
-    <div role="tabpanel" class="tab-pane active" id="eqlogictab">
-      <br />
-      <div class="row">
-        <div class="col-lg-7">
-          <form class="form-horizontal">
-            <fieldset>
+  <div class="col-xs-12 eqLogic" style="display: none;">
+    <div class="input-group pull-right" style="display:inline-flex">
+      <span class="input-group-btn">
+        <a class="btn btn-danger btn-sm roundedLeft" id="bt_autoDetectModule"><i class="fas fa-search" title="{{Recréer les commandes}}"></i> {{Recréer les commandes}}
+        </a><a class="btn btn-warning btn-sm haspairing" id="bt_pairing"><i class="fas fa-barcode" title="{{Trame pairing}}"></i> {{Trame pairing}}
+        </a><a class="btn btn-default btn-sm eqLogicAction" data-action="configure"><i class="fas fa-cogs"></i><span class="hidden-xs"> {{Configuration avancée}}</span>
+        </a><a class="btn btn-sm btn-success eqLogicAction" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}
+        </a><a class="btn btn-danger btn-sm eqLogicAction roundedRight" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}
+        </a>
+      </span>
+    </div>
+    <ul class="nav nav-tabs" role="tablist">
+      <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fas fa-arrow-circle-left"></i></a></li>
+      <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer-alt"></i> {{Equipement}}</a></li>
+      <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a></li>
+    </ul>
+    <div class="tab-content">
+      <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+        <form class="form-horizontal">
+          <fieldset>
+            <div class="col-lg-7">
               <div class="form-group">
                 <label class="col-sm-3 control-label">{{Nom du device}}</label>
                 <div class="col-sm-7">
@@ -179,18 +191,13 @@ if (config::byKey('include_mode', 'beagle', 0) == 1) {
 
                 </div>
               </div>
-            </fieldset>
-          </form>
-        </div>
-        <div class="col-lg-5">
-          <div class="form-group">
-            <center>
-              <img src="core/img/no_image.gif" data-original=".jpg" id="img_device" class="img-responsive" style="max-height : 250px;" />
-            </center>
-          </div>
-          <form class="form-horizontal">
-            <fieldset>
-
+            </div>
+            <div class="col-lg-5">
+              <div class="form-group">
+                <center>
+                  <img src="core/img/no_image.gif" data-original=".jpg" id="img_device" class="img-responsive" style="max-height : 250px;" />
+                </center>
+              </div>
               <div class="form-group">
                 <label class="col-sm-3 control-label">{{Modèle}}</label>
                 <div class="col-sm-3">
@@ -217,33 +224,28 @@ if (config::byKey('include_mode', 'beagle', 0) == 1) {
                   <span class="eqLogicAttr label label-info" style="font-size:1em;cursor: default;" data-l1key="configuration" data-l2key="paired"></span>
                 </div>
               </div>
-            </fieldset>
-          </form>
+            </div>
           </fieldset>
-          </form>
-        </div>
+        </form>
       </div>
-
     </div>
     <div role="tabpanel" class="tab-pane" id="commandtab">
       <a class="btn btn-success btn-sm cmdAction pull-right" data-action="add" style="margin-top:5px;"><i class="fa fa-plus-circle"></i> {{Ajouter une commande}}</a><br /><br />
       <table id="table_cmd" class="table table-bordered table-condensed">
         <thead>
           <tr>
-            <th style="width: 20%;">{{Nom}}</th>
-            <th style="width: 25%;">Type</th>
-            <th style="width: 30%;">{{Paramètres}}</th>
-            <th style="width: 25%;"></th>
+            <th style="min-width:200px;width:350px;">{{Nom}}</th>
+            <th>{{Type}}</th>
+            <th style="min-width:260px;">{{Options}}</th>
+            <th>{{Etat}}</th>
+            <th style="min-width:80px;width:200px;">{{Actions}}</th>
           </tr>
         </thead>
         <tbody>
-
         </tbody>
       </table>
-
     </div>
   </div>
-
 </div>
 </div>
 
